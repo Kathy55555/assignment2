4865d12533a660cb15fc00ae524dfa018b354925
Kathy55555 <kathy.song@student.uts.edu.au>
2026-05-20 20:15:17 +1000

    fix issue

--- server/models/StudyHistory.js
+++ server/models/StudyHistory.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
+
 const StudyHistorySchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-
   flashcardId: { type: mongoose.Schema.Types.ObjectId, ref: "Flashcard" },
-
   question: String,
   answer: String,
-
   completedAt: { type: Date, default: Date.now }
 });
+
+module.exports = mongoose.model("StudyHistory", StudyHistorySchema);
